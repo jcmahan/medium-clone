834a69199c6e22f2b1a3862830b360af1bc46d88
James Mahan <36861942+jcmahan@users.noreply.github.com>
2018-07-31 17:43:17 -0700

    working with node, debugging

--- server/app.js
+++ server/app.js
@@ -10,3 +10,38 @@ const app = express()
 const router = express.Router
 const url = process.env.MONGODB_URI ||
 'mongodb://localhost:27017/medium'
+
+/**
+ * configure Cloudinary
+ */
+cloudinary.config({
+    cloud_name: 'YOUR CLOUDINARY NAME HERE',
+    api_key: 'YOUR CLOUDINARY API KEY HERE', 
+    api_secret: 'your cloudinary api secret here'
+})
+/**
+ * connect to MongoDB datastore
+ */
+try {
+    mongoose.connect(url, {
+        //useMongoClient:true
+    })
+} catch (error) {
+
+}
+let port = 5000 || process.env.PORT
+
+/** set up routes {API endpoints} */
+routes(router)
+
+/** set up Middlewares */
+app.use(cors())
+app.use(bodyParser.json())
+app.use(helmet())
+//app.use('static', express.static(path.join(__dirname, 'static')))
+app.use('api', router)
+
+/** start server */
+app.listen(port, () => {
+    console.log(`Server started at port ${port}`);
+});
